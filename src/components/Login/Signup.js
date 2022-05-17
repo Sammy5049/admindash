df963dc488110caaa2a69df40e644ea0f52dfc01
Vhickjr <victormathias001@gmail.com>
2022-05-17 22:20:15 +0100

    Sign up

--- src/components/Login/Signup.js
+++ src/components/Login/Signup.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import styled from 'styled-components'
 import './signin.css'
 // import { ToastContainer, toast } from "react-toastify";
 // import { Link } from "react-router-dom";
